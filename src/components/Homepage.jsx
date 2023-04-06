6f751e03e8c31f797c1976fffba7babd687d7de2
Carlo Mirador Jr <CEM.code404@gmail.com>
2023-04-06 20:22:54 +0800

    Added a parallax on the homepage

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import BookCardList from "./common/BookCardList.jsx";
 import Footer from "./common/Footer.jsx";
 import Parallax from "./common/Parallax.jsx";
 
 const HomePage = () => {
+  const scrollRef = useRef(null);
+  const [parallaxState, setParallaxState] = useState(0);
   return (
-    <div className="home-page">
-      <Parallax />
+    <div
+      className="home-page"
+      ref={scrollRef}
+      onScroll={() => {
+        setParallaxState(scrollRef.current.scrollTop);
+      }}
+    >
+      <Parallax parallaxState={parallaxState} />
       <BookCardList title={"TRENDING RIGHT NOW..."} />
       <BookCardList title={"NEW UPLOADS..."} />
       <div className="home-page__diagonal-separator"></div>
--- src/components/common/Parallax.jsx
+++ src/components/common/Parallax.jsx
@@ -1,23 +1,52 @@
 import React from "react";
+import welcomeSign from "../../assets/Foreground-welcome.svg";
 import books from "../../assets/Foreground-books.svg";
 import background from "../../assets/Background.svg";
+import closeEyesBookman from "../../assets/Close_eyes_bookman.svg";
 import bookman from "../../assets/Bookman.svg";
-import welcomeSign from "../../assets/Foreground-welcome.svg";
 
-const Parallax = () => {
+const Parallax = ({ parallaxState }) => {
   return (
     <div className="parallax">
-      <object type="image/svg+xml" data={background}>
+      <object
+        type="image/svg+xml"
+        data={background}
+        style={{ top: -(parallaxState * 0.1) + "px" }}
+      >
         Background
       </object>
-      <object type="image/svg+xml" data={bookman}>
-        Background
+      <object
+        type="image/svg+xml"
+        data={closeEyesBookman}
+        style={{ top: -(parallaxState * 0.3) + "px" }}
+      >
+        Bookman
       </object>
-      <object type="image/svg+xml" data={books}>
-        Background
+      <object
+        type="image/svg+xml"
+        data={bookman}
+        className="animate--blink"
+        style={{ top: -(parallaxState * 0.3) + "px" }}
+      >
+        Bookman
       </object>
-      <object type="image/svg+xml" data={welcomeSign}>
-        Background
+      <object
+        type="image/svg+xml"
+        data={books}
+        style={{ top: -(parallaxState * 0.6) + "px" }}
+      >
+        Books
+      </object>
+      <object
+        type="image/svg+xml"
+        data={welcomeSign}
+        style={{
+          top: -(parallaxState * 0.6) + "px",
+          right: -(parallaxState * 0.6) + "px",
+        }}
+        className="animate--swing"
+      >
+        Welcome Sign
       </object>
     </div>
   );

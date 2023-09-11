6eaffdb41c785a1da21ef62d4d09a07bd0a71c71
Carlo Mirador Jr <CEM.code404@gmail.com>
2023-09-11 15:15:32 +0800

    Changed reviews url

--- src/services/reviewService.js
+++ src/services/reviewService.js
@@ -2,7 +2,7 @@ import http from "./httpService";
 
 function getReviews(cb) {
   http
-    .get("http://localhost:8000/api/reviews")
+    .get("http://localhost:8000/api/review/getReviews")
     .then((reviews) => {
       let err;
       cb(reviews, err);
